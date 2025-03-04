886358aee3d81ac369ae60e8b562ef951f17d4ae
EvoSix <49368909+EvoSix@users.noreply.github.com>
2025-03-04 23:32:14 +0100

    Ajout du test

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -11,15 +11,36 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import { Router } from '@angular/router';
+import { LoginRequest } from '../../interfaces/loginRequest.interface';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
+import { of, throwError } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 
+class MockAuthService {
+  login(loginRequest: LoginRequest) {
+    return of({
+      token: 'fake-token',
+      id: 1,
+      username: 'user',
+      firstName: 'Will',
+      lastName: 'Smith',
+      type: 'user'
+    } as SessionInformation);
+  }
+}
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
+  let router: Router;
+  let authService: MockAuthService;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [
+        SessionService,
+        { provide: AuthService, useClass: MockAuthService }
+      ],
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
@@ -33,10 +54,35 @@ describe('LoginComponent', () => {
       .compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should redirect to /sessions after successful login', () => {
+    const navigateSpy = jest.spyOn(router, 'navigate');
+
+    component.form.controls['email'].setValue('test@example.com');
+    component.form.controls['password'].setValue('password123');
+
+    component.submit();
+
+    expect(navigateSpy).toHaveBeenCalledWith(['/sessions']);
+  });
+
+  it('should set onError to true if login fails', () => {
+    jest.spyOn(authService, 'login').mockReturnValue(throwError(() => new Error('Login failed')));
+
+    component.form.controls['email'].setValue('wrong@example.com');
+    component.form.controls['password'].setValue('wrongpassword');
+
+    component.submit();
+
+    expect(component.onError).toBe(true);
+  });
+
 });
