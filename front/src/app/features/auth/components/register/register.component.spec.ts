e71ce59abf9921b3c567ed415a4d8a0664ff434a
EvoSix <49368909+EvoSix@users.noreply.github.com>
2025-03-27 20:26:31 +0100

    Restauration d'un fichier

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -9,14 +9,26 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import { RegisterRequest } from '../../interfaces/registerRequest.interface';
+import { Router } from '@angular/router';
+import { Observable, of, throwError } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
+
+class MockAuthService {
+  register = (req: RegisterRequest): Observable<void> => of(void 0);
+}
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let router: Router;
+  let authService: MockAuthService;
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
+      providers: [{ provide: AuthService, useClass: MockAuthService }],
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
@@ -29,12 +41,57 @@ describe('RegisterComponent', () => {
     })
       .compileComponents();
 
-    fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      fixture = TestBed.createComponent(RegisterComponent);
+      component = fixture.componentInstance;
+      authService = TestBed.inject(AuthService);
+      router = TestBed.inject(Router);
+      fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it("should redirect to /login after successful register", () => {
+    const navigateSpy = jest.spyOn(router, "navigate");
+
+    component.form.controls["firstName"].setValue("John");
+    component.form.controls["lastName"].setValue("Doe");
+    component.form.controls["email"].setValue("john@example.com");
+    component.form.controls["password"].setValue("securePass");
+
+    component.submit();
+
+    expect(navigateSpy).toHaveBeenCalledWith(["/login"]);
+  });
+
+  it("should set onError to true if register fails", () => {
+    jest
+      .spyOn(authService, "register")
+      .mockReturnValue(throwError(() => new Error("Register failed")));
+
+    component.form.controls["firstName"].setValue("Error");
+    component.form.controls["lastName"].setValue("User");
+    component.form.controls["email"].setValue("error@example.com");
+    component.form.controls["password"].setValue("123456");
+
+    component.submit();
+
+    expect(component.onError).toBe(true);
+  });
+
+  it("should set onError to true if fields are empty", () => {
+    jest
+      .spyOn(authService, "register")
+      .mockReturnValue(throwError(() => new Error("Error: Fields are empty")));
+
+    component.form.controls["firstName"].setValue("");
+    component.form.controls["lastName"].setValue("");
+    component.form.controls["email"].setValue("");
+    component.form.controls["password"].setValue("");
+
+    component.submit();
+
+    expect(component.onError).toBe(true);
+  });
 });
+
